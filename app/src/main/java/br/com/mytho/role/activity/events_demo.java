a282ca09658102b12c1a3112c656c38272b909d2
maxalves <max.phazen@gmail.com>
2016-07-06 18:26:10 -0300

    Events for test activity

---
+++ app/src/main/java/br/com/mytho/role/activity/events_demo.java
@@ -0,0 +1,15 @@
+package br.com.mytho.role.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.mytho.role.R;
+
+public class events_demo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events_demo);
+    }
+}
