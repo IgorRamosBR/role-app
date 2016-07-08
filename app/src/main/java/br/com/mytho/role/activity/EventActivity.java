92f5b1427e570c11898b03641455026656aa8eda
IgorRamosBR <igorrpestana@yahoo.com.br>
2016-07-08 01:34:33 -0300

    Botao Ver Mais expandivel a descricao do evento

--- app/src/main/java/br/com/mytho/role/activity/EventActivity.java
+++ app/src/main/java/br/com/mytho/role/activity/EventActivity.java
@@ -1,11 +1,13 @@
 package br.com.mytho.role.activity;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,8 +24,11 @@ public class EventActivity extends AppCompatActivity {
     @BindView(R.id.tv_local) TextView mEventLocal;
     @BindView(R.id.tv_aboutt) TextView mAbout;
     @BindView(R.id.iv_event) ImageView mEventPicture;
+    @BindView(R.id.tv_seemore) TextView mSeemore;
+
     private Event event;
 
+    private boolean expandable = false, expand = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +44,44 @@ public class EventActivity extends AppCompatActivity {
         event = receiveEvent();
         fillViews();
 
+        aboutExpandable();
+
     }
+
+    //ABOUT VIEW EXPANDABLE
+    private void aboutExpandable(){
+        mAbout.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if(expandable){
+                    expandable = false;
+                    if(mAbout.getLineCount() > 4) {
+                        mSeemore.setVisibility(View.VISIBLE);
+                        ObjectAnimator animation = ObjectAnimator.ofInt(mAbout, "maxLines", 4);
+                        animation.setDuration(0).start();
+                    }
+                }
+            }
+        });
+
+        mSeemore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!expand) {
+                    expand = true;
+                    ObjectAnimator animation = ObjectAnimator.ofInt(mAbout, "maxLines", 40);
+                    animation.setDuration(100).start();
+                    mSeemore.setText("Ver menos");
+                } else {
+                    expand = false;
+                    ObjectAnimator animation = ObjectAnimator.ofInt(mAbout, "maxLines", 4);
+                    animation.setDuration(100).start();
+                    mSeemore.setText("Ver mais");
+                }
+            }
+        });
+    }
+
 
 
     //AFTER, THIS METHOD WILL GET THE RESULT FROM ANOTHER ACTIVITY
