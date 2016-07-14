e98b1c2cc79e5776413203ed937c305abcdbaba1
IgorRamosBR <igorrpestana@yahoo.com.br>
2016-07-14 16:39:11 -0300

    Refatoracao da mainactivity

--- app/src/main/java/br/com/mytho/role/activity/MainActivity.java
+++ app/src/main/java/br/com/mytho/role/activity/MainActivity.java
@@ -50,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.tabanim_tabs)
     TabLayout tabLayout;
 
+    private List<Event> mEvents;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,24 +63,9 @@ public class MainActivity extends AppCompatActivity {
 
         prepareRecyclerView();
 
-        Event event1 = new Event();
-        event1.setTitle("Big Title for event title size test - With Darker BG");
-        event1.setImageLink("android.resource://br.com.mytho.role/" + R.drawable.event_image3);
-        event1.setAbout("Etiam posuere quam ac quam. Maecenas aliquet accumsan leo. Etiam posuere quam ac quam. Maecenas aliquet accumsan leo.");
+        fillList();
 
-        Event event2 = new Event();
-        event2.setTitle("Medium Title with whiter image");
-        event2.setImageLink("android.resource://br.com.mytho.role/" + R.drawable.event_image2);
-        event2.setAbout("Etiam posuere quam ac quam. Maecenas aliquet accumsan leo.");
-
-        Event event3 = new Event();
-        event3.setTitle("Small Title");
-        event3.setImageLink("android.resource://br.com.mytho.role/" + R.drawable.event_image1);
-        event3.setAbout("Etiam posuere quam.");
-
-        List<Event> events = Arrays.asList(event1, event2, event3);
-
-        recyclerView.setAdapter(new RecyclerEventsAdapter(events));
+        prepareNavigationDrawer();
 
         OAuthAccessTokenService oAuthAccessTokenService = new OAuthAccessTokenService.Builder().build();
         Call<AccessToken> callForAccessToken = oAuthAccessTokenService.getAccessToken("public-area", "client_credentials");
@@ -97,6 +84,35 @@ public class MainActivity extends AppCompatActivity {
                 t.printStackTrace();
             }
         });
+
+
+    }
+
+    public void process(Response<AccessToken> response) {
+        AccessToken token = response.body();
+        EventService service = new EventService.Builder().accessToken(token).build();
+        Call<List<Event>> callForList = service.list();
+        callForList.enqueue(new Callback<List<Event>>() {
+            @Override
+            public void onResponse(Call<List<Event>> call, Response<List<Event>> response) {
+                if(!response.isSuccessful()) {
+                    // error handling
+                } else {
+                    for(Event event : response.body()) {
+                        Toast.makeText(MainActivity.this, event.getTitle() + " - " + new SimpleDateFormat("dd/MM/yyyy HH:mm").format(event.getDate().getTime()), Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Event>> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
+
+    }
+
+    private void prepareNavigationDrawer() {
 
         AccountHeader accountHeader = new AccountHeaderBuilder()
                 .withActivity(this)
@@ -156,31 +172,6 @@ public class MainActivity extends AppCompatActivity {
                     }
                 })
                 .build();
-
-    }
-
-    public void process(Response<AccessToken> response) {
-        AccessToken token = response.body();
-        EventService service = new EventService.Builder().accessToken(token).build();
-        Call<List<Event>> callForList = service.list();
-        callForList.enqueue(new Callback<List<Event>>() {
-            @Override
-            public void onResponse(Call<List<Event>> call, Response<List<Event>> response) {
-                if(!response.isSuccessful()) {
-                    // error handling
-                } else {
-                    for(Event event : response.body()) {
-                        Toast.makeText(MainActivity.this, event.getTitle() + " - " + new SimpleDateFormat("dd/MM/yyyy HH:mm").format(event.getDate().getTime()), Toast.LENGTH_LONG).show();
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<Event>> call, Throwable t) {
-                t.printStackTrace();
-            }
-        });
-
     }
 
     public void prepareTabs() {
@@ -190,7 +181,27 @@ public class MainActivity extends AppCompatActivity {
         assembler.withIcon(R.drawable.ic_filter).add();
     }
 
+    private void fillList() {
+        Event event1 = new Event();
+        event1.setTitle("Big Title for event title size test - With Darker BG");
+        event1.setImageLink("android.resource://br.com.mytho.role/" + R.drawable.event_image3);
+        event1.setAbout("Etiam posuere quam ac quam. Maecenas aliquet accumsan leo. Etiam posuere quam ac quam. Maecenas aliquet accumsan leo.");
+
+        Event event2 = new Event();
+        event2.setTitle("Medium Title with whiter image");
+        event2.setImageLink("android.resource://br.com.mytho.role/" + R.drawable.event_image2);
+        event2.setAbout("Etiam posuere quam ac quam. Maecenas aliquet accumsan leo.");
+
+        Event event3 = new Event();
+        event3.setTitle("Small Title");
+        event3.setImageLink("android.resource://br.com.mytho.role/" + R.drawable.event_image1);
+        event3.setAbout("Etiam posuere quam.");
+
+        mEvents = Arrays.asList(event1, event2, event3);
+    }
+
     public void prepareRecyclerView() {
+        recyclerView.setAdapter(new RecyclerEventsAdapter(mEvents));
         recyclerView.setHasFixedSize(true);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(mLayoutManager);
