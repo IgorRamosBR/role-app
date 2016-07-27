09d1e211486b3ab02fb37389027fdf359c32c446
IgorRamosBR <igorrpestana@yahoo.com.br>
2016-07-27 13:11:07 -0300

    Click

--- app/src/main/java/br/com/mytho/role/adapter/RecyclerEventsAdapter.java
+++ app/src/main/java/br/com/mytho/role/adapter/RecyclerEventsAdapter.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import android.net.Uri;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -31,20 +33,22 @@ public class RecyclerEventsAdapter extends RecyclerView.Adapter<RecyclerEventsAd
         this.events = events;
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder{
+    static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         public ImageView itemImage;
         public TextView itemName;
         public TextView itemDescription;
+        public TextView itemId;
+        public IMyViewHolderClicks mListener;
 
-        public ViewHolder(View itemView) {
+        public ViewHolder(View itemView, IMyViewHolderClicks listener) {
             super(itemView);
-
+            mListener = listener;
             itemImage = (ImageView)itemView.findViewById(R.id.eventImage);
             itemName = (TextView)itemView.findViewById(R.id.eventName);
             itemDescription = (TextView)itemView.findViewById(R.id.eventSubtitle);
-
-            itemView.setOnClickListener(new View.OnClickListener() {
+            itemId = (TextView) itemView.findViewById(R.id.eventId);
+            /*itemView.setOnClickListener(new View.OnClickListener() {
                 @Override public void onClick(View v) {
 
                     Snackbar.make(v, "Click detected on item ",
@@ -52,19 +56,36 @@ public class RecyclerEventsAdapter extends RecyclerView.Adapter<RecyclerEventsAd
                             .setAction("Action", null).show();
 
                 }
-            });
+            });*/
+            itemView.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View v) {
+            mListener.onEvent(Long.parseLong(itemId.getText().toString()));
+        }
+
+        public interface IMyViewHolderClicks {
+            void onEvent(long id);
+        }
+
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+    public ViewHolder onCreateViewHolder(final ViewGroup viewGroup, int i) {
         View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item, viewGroup, false);
-        ViewHolder viewHolder = new ViewHolder(v);
+        ViewHolder viewHolder = new ViewHolder(v, new RecyclerEventsAdapter.ViewHolder.IMyViewHolderClicks() {
+            @Override
+            public void onEvent(long id) {
+                Log.i("CLICK", "id: " + id);
+            }
+        });
         return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, int i) {
+        viewHolder.itemId.setText(Long.toString(events.get(i).getId()));
         viewHolder.itemName.setText(events.get(i).getTitle());
         viewHolder.itemDescription.setText(events.get(i).getAbout());
 //        viewHolder.itemImage.setImageURI(Uri.parse(events.get(i).getImageLink()));
